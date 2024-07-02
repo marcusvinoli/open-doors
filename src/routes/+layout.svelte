83e71aa37e74530f0ab82f9869afbb42feae3428
Marcus V M Oliveira <marcusvinoli@gmail.com>
2024-07-01 22:59:34 -0300

    Adds basic layout for application.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,59 @@
 <script lang="ts">
     import "../app.pcss";
+    import ToolBar from "$lib/components/global/toolbar/ToolBar.svelte";
+    import StatusBar from "$lib/components/global/status_bar/StatusBar.svelte";
+    import TabHeader from "$lib/components/global/tabs/TabHeader.svelte";
+    
+    import type {TabData} from "$lib/components/global/tabs/TabData";
+
+    // Toolbar Module
+    let showToolbar: boolean = false;
+    
+    // Tabs Module
+    let tabs: Array<TabData> = [];
+    
+    let activeTab: number | null = 1;
+
+    function closeTab(event: any) {
+        const tabId = event.detail.id;
+        var newTabs: TabData[] = tabs;
+        console.log("Closing tab "+tabId);
+        console.log("Active tab "+activeTab);
+
+        const indexToRemove = newTabs.findIndex((tab) => {
+            return(tab.id === tabId);
+        });
+
+        if(indexToRemove < 0) {
+            return;
+        }
+
+        if(newTabs[indexToRemove].id === activeTab) {
+            if (indexToRemove === 0) {
+                activeTab = newTabs[indexToRemove+1] ? newTabs[indexToRemove+1].id : null;
+            } else {
+            }
+            activeTab = (newTabs[indexToRemove-1]) ? newTabs[indexToRemove-1].id : null;
+        }
+
+        newTabs.splice(indexToRemove, 1);
+        
+        tabs = newTabs;
+    }
+    
+    function openTab(event: any) {
+        activeTab = event.detail.id;
+    }
+
 </script>
-<slot/>
+
+<div class="flex flex-col w-full h-full bg-slate-200">
+    {#if showToolbar}
+    <ToolBar />
+    {/if}
+    <TabHeader tabs={tabs} on:close={closeTab} on:open={openTab} active={activeTab}/>
+    <div class="grow my-1">
+        <slot />
+    </div>
+    <StatusBar />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,69 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    //import { open } from "@tauri-apps/cli";
+
+    import * as Dialog from "$lib/components/ui/dialog";
+
+    import OpenDoorsLogo from "$lib/assets/open-doors-logo-71717a.svg";
+    import Button from "$lib/components/ui/button/button.svelte";
+
+    let selectedFolder = "";
+
+    /* async function selectFolder() {
+        const folder = await open({
+            directory: true,
+            multiple: false
+        });
+        if (folder) {
+            selectedFolder = folder;
+        }
+    } */
+    
+</script>
+
+<div class="flex flex-col items-center justify-center w-full h-full pb-48 text-zinc-600">
+    <div class="w-[180px] mb-6">
+        <img src={OpenDoorsLogo} alt="OpenDOORs" />
+    </div>
+    <div class="text-center leading-10 mb-4">
+        <h1 class="text-2xl font-bold mb-1">Welcome to OpenDOORs.</h1>
+        <p class="text-xl">A simple tool that open doors for yours projects.</p>
+        <p class="font-semi italic text-[0.95em]">Let's start opening, creating or cloning a repository.</p>
+    </div>
+    <div>
+        <div class="w-96 flex gap-5 justify-center">
+            <Dialog.Root>
+                <Dialog.Trigger><Button>Open</Button></Dialog.Trigger>
+                <Dialog.Content>
+                    <Dialog.Header>
+                        <Dialog.Title>Open</Dialog.Title>
+                        <Dialog.Description>
+                        </Dialog.Description>
+                    </Dialog.Header>
+                </Dialog.Content>
+            </Dialog.Root>
+            <Dialog.Root>
+                <Dialog.Trigger><Button variant="secondary">Clone</Button></Dialog.Trigger>
+                <Dialog.Content>
+                    <Dialog.Header>
+                        <Dialog.Title>Open</Dialog.Title>
+                        <Dialog.Description>
+                            <p>Insert the Git URL of the Repository and select a local folder. Then select a name for this repository.</p>
+                        </Dialog.Description>
+                    </Dialog.Header>
+                </Dialog.Content>
+            </Dialog.Root>
+            <Dialog.Root>
+                <Dialog.Trigger><Button variant="secondary">Clone</Button></Dialog.Trigger>
+                <Dialog.Content>
+                    <Dialog.Header>
+                        <Dialog.Title>Open</Dialog.Title>
+                        <Dialog.Description>
+                        </Dialog.Description>
+                    </Dialog.Header>
+                </Dialog.Content>
+            </Dialog.Root>
+                
+            
+        </div>
+    </div>
+</div>
