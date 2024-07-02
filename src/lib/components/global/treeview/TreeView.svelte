5c46b5400e29c35481115fe428e44bc927b242e5
Marcus V M Oliveira <marcusvinoli@gmail.com>
2024-07-01 22:57:05 -0300

    Adds Treeview component preview.

---
+++ src/lib/components/global/treeview/TreeView.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { createEventDispatcher } from 'svelte';
+    import Tree from "./Tree.svelte";
+    import type { TreeItemData } from "./TreeItemData";
+
+    const dispatch = createEventDispatcher();
+
+    export let treeItems: TreeItemData[] = []
+
+    function handleClick(event: any) {
+        console.log(event.detail);
+    }
+
+</script>
+
+<div>
+    <Tree items={treeItems} on:click={handleClick}/>
+</div>
