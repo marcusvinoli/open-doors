fb4ae226995c4b83c4dbdfeff10771cfa8f278be
Marcus V M Oliveira <marcusvinoli@gmail.com>
2024-07-07 15:40:58 -0300

    Changes repository functions

--- src-tauri/src/core/repo/repository.rs
+++ src-tauri/src/core/repo/repository.rs
@@ -3,81 +3,94 @@ use std::path::PathBuf;
 use serde::{Serialize, Deserialize};
 use super::middleware as mid;
 
-use crate::core::repo::definitions::OD_MANIFEST_FILE_NAME;
 use crate::core::{RepositoryError, TreeItem, TreeItemType};
 use crate::git::{self, GitError};
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
-pub struct RespositoryManifest {
+pub struct RepositoryManifest {
     pub name: String,
-    pub path: PathBuf,
     #[serde(skip_serializing_if="Option::is_none")]
     pub remote: Option<String>,
 }
 
 #[derive(Clone, Debug, Default, Serialize, Deserialize)]
-pub struct Respository{
-    pub manifest: RespositoryManifest,
+pub struct Repository{
+    pub manifest: RepositoryManifest,
     pub structure: TreeItem,
+    pub path: PathBuf,
 }
 
-impl Respository {
-    pub fn create(path: &PathBuf, name: String, remote: Option<String>) -> Result<Respository, RepositoryError> {         
+impl Repository {
+    pub fn create(path: PathBuf, name: String, remote: Option<String>) -> Result<Repository, RepositoryError> {         
         if git::is_git_repository(path.display().to_string())? {
             return Err(RepositoryError::GitError(GitError::RepositoryNotEmpty));
         }
 
-        let man: RespositoryManifest = RespositoryManifest {
+        let man: RepositoryManifest = RepositoryManifest {
             name: name.clone(),
-            path: path.clone(),
             remote,
         };
-
+        
         let tree: TreeItem = TreeItem {
             item_type: TreeItemType::Repository,
-            name,
-            path: path.clone(),
+            path: PathBuf::from("./"),
             children: Vec::new(),
+            name,
         };
-
-        mid::create_manifest(&man)?;
-        mid::create_structure_file(&man.path, &tree)?;
-
+        
+        mid::create_manifest_file(&path, &man)?;
+        mid::create_structure_file(&path, &tree)?;
+        
         git::init(&path.display().to_string())?;
         git::add_all(&path.display().to_string())?;
         git::commit(&path.display().to_string(), "OpenDOORs repository initiated.")?;
-
-        Ok(Respository { 
+        
+        Ok(Repository { 
+            path: path,
             manifest: man, 
             structure: tree
         })
     }
     
-    pub fn read(path: &PathBuf) -> Result<Respository, RepositoryError> {
+    pub fn read(path: PathBuf) -> Result<Repository, RepositoryError> {
         if !(git::is_git_repository(path.display().to_string())?) {
             return Err(RepositoryError::GitError(GitError::RepositoryNotEmpty));
         }
         
-        let man: RespositoryManifest = mid::read_manifest(path)?;
-        let tree: TreeItem = mid::read_structure_file(&man.path)?;
+        let man: RepositoryManifest = mid::read_manifest_file(&path)?;
+        let tree: TreeItem = mid::read_structure_file(&path)?;
         
-        Ok(Respository { 
+        Ok(Repository { 
+            path,
             manifest: man,
             structure: tree,
         })
     }
     
-    pub fn update(&self) -> Result<(), RepositoryError> {
-        if !(git::is_git_repository(&self.manifest.path.display().to_string())?) {
+    pub fn update_manifest(path: PathBuf, man: RepositoryManifest) -> Result<(), RepositoryError> {
+        if !(git::is_git_repository(&path.display().to_string())?) {
             return Err(RepositoryError::GitError(GitError::RepositoryNotEmpty));
         }
 
-        mid::update_manifest(&self.manifest)?;
+        mid::update_manifest_file(&path, &man)?;
 
         return Ok(());
     }
     
-    pub fn delete(self) -> Result<(), RepositoryError> {
-        mid::delete_manifest(self.manifest)
+    pub fn update_structure(path: PathBuf, tree: TreeItem) -> Result<(), RepositoryError> {
+        if !(git::is_git_repository(&path.display().to_string())?) {
+            return Err(RepositoryError::GitError(GitError::RepositoryNotEmpty));
+        }
+
+        mid::update_structure_file(&path, &tree)?;
+
+        return Ok(());
+    }
+
+    pub fn delete(path: PathBuf) -> Result<(), RepositoryError> {
+        todo!("Delete operation not Implemented.");
+        // mid::delete_manifest_file(&path)?;
+        // mid::delete_structure_file(&path)?;
+        // Ok(())
     }
 }
