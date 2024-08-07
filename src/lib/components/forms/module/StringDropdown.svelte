bad142c40321a8f55298dec8f3e93307b0796c8e
Marcus Vinicius Miguel de Oliveira <marcusvinoli@gmail.com>
2024-08-07 01:35:44 -0300

    Fixes string dropdown component

--- src/lib/components/forms/module/StringDropdown.svelte
+++ src/lib/components/forms/module/StringDropdown.svelte
@@ -3,19 +3,19 @@
     import { cn } from "$lib/utils.js"; 
     import { tick } from "svelte";
     import { Button } from "$lib/components/ui/button/index.js";
-    import * as Command from "$lib/components/ui/command/index.js";
-    import * as Popover from "$lib/components/ui/popover/index.js";
     import { ChevronDown } from "lucide-svelte";
     import { createEventDispatcher } from "svelte";
+    import * as Command from "$lib/components/ui/command/index.js";
+    import * as Popover from "$lib/components/ui/popover/index.js";
 
     export let options: string[] = [];
-    export let selected: string = options[0] ? options[0] : "";
-    let selection: string;
+    export let selected: string = "";
+    export let placeholder: string = "Select...";
 
     const dispatch = createEventDispatcher();
 
     function handleSelection(select: string) {
-        selection = select;
+        selected = select;
         dispatch('select', {item: selected})
     }
 
@@ -29,25 +29,33 @@
         document.getElementById(triggerId)?.focus();
         });
     }
+
+	$: {
+		if(options) {
+			selected = "";
+		}
+	}
 </script>
 
 <Popover.Root bind:open={openCombobox} let:ids>
     <Popover.Trigger asChild let:builder>
       <Button builders={[builder]} variant="outline" role="combobox" aria-expanded={openCombobox} class="justify-between w-full">
-        {selection}
+        {selected != "" ? selected : placeholder}
       <ChevronDown class="ml-2 h-4 w-4 shrink-0 opacity-50" />
       </Button>
     </Popover.Trigger>
-    <Popover.Content class="p-0 min-w-12">
-      <Command.Root>
-          <Command.Group class="min-w-12">
-            {#each options as option}
-              <Command.Item value={option} onSelect={() => { closeAndFocusTrigger(ids.trigger); handleSelection(option); }}>
-                <Check class={cn("mr-2 h-4 w-4", !(option === selected) && "text-transparent" )}/> 
-                <span class="pl-1">{option}</span>
-              </Command.Item>
-            {/each}
-          </Command.Group>
-      </Command.Root>
-    </Popover.Content>
-  </Popover.Root>
+    <Popover.Content class="p-0 min-w-12 max-h-32">
+      	<Command.Root>
+        	<Command.Group class="min-w-12 max-h-32 overflow-scroll">
+              {#each options as option}
+                <Command.Item value={option} onSelect={() => { closeAndFocusTrigger(ids.trigger); handleSelection(option); }}>
+                  <Check class={cn("mr-2 h-4 w-4", !(option === selected) && "text-transparent" )}/> 
+                  <span class="pl-1">{option}</span>
+                </Command.Item>
+              {/each}
+            </Command.Group>
+		</Command.Root>
+	</Popover.Content>
+</Popover.Root>
+
+      
